a8e17c4b997045e86682e0274e63c0de5ba93aaa
Vivi <vachetvirginie@gmail.com>
2023-08-17 22:28:23 +0200

    feat: add few style

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <input v-model="query" placeholder="Entrez le nom du produit" />
-    <button @click="fetchProductsByName">Chercher</button>
+  <div class="container">
+    <div class="search-section">
+      <input v-model="query" placeholder="Entrez le nom du produit" />
+      <button @click="fetchProductsByName">Chercher</button>
+    </div>
     <div v-if="records.length" style="margin-top: 20px">
       <h3>Historique des protéines :</h3>
       <ul>
         <li v-for="record in records" :key="record.id">
-          {{ record.date }}: {{ record.protein }} g pour {{ record.quantity }}g
+          <h3>{{ record.productName }} ({{ record.date }})</h3>
+          {{ record.protein }} g pour {{ record.quantity }}g
           <button @click="beginEditRecord(record)">Modifier</button>
           <button @click="deleteRecord(record.id)">Supprimer</button>
           <div v-if="recordToEdit && recordToEdit.id === record.id">
@@ -29,27 +32,18 @@
       <li v-for="product in products" :key="product.id">
         <h2>{{ product.product_name }}</h2>
         <img
+          class="img-product"
           :src="product.image_url"
           alt="Image du produit"
           v-if="product.image_url"
           @click="displayProtein(product)"
-          style="cursor: pointer"
         />
       </li>
     </ul>
-    <div
-      v-if="showModal"
-      style="
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background: white;
-        padding: 20px;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-      "
-    >
+
+    <div v-if="showModal" class="overlay"></div>
+
+    <div v-if="showModal" class="modal">
       Protein per 100g: {{ proteinValue }} g
       <div style="margin-top: 10px">
         <label for="quantity">Quantité du produit (en g) :</label>
@@ -112,6 +106,7 @@ export default {
     displayProtein(product) {
       this.proteinValue = product.nutriments.proteins_100g;
       this.showModal = true;
+      this.productName = product.product_name;
     },
     calculateProteinForQuantity() {
       if (this.quantity && this.proteinValue) {
@@ -123,6 +118,7 @@ export default {
     storeRecord() {
       const record = {
         id: Date.now().toString(),
+        productName: this.productName,
         date: new Date().toISOString().slice(0, 10),
         protein: this.calculatedProtein.toFixed(2),
         proteinPer100g: this.proteinValue,
@@ -130,6 +126,7 @@ export default {
       };
       this.records.push(record);
       localStorage.setItem("proteinRecords", JSON.stringify(this.records));
+      this.closePopup();
     },
     loadRecords() {
       const storedRecords = localStorage.getItem("proteinRecords");
@@ -178,4 +175,99 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  font-family: "Arial", sans-serif;
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #f9f9f9;
+}
+
+h2 {
+  text-align: center;
+}
+
+h3 {
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+}
+
+input,
+button {
+  margin: 10px 0;
+  padding: 8px 12px;
+  font-size: 16px;
+}
+
+button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 15px;
+  margin: 10px 0;
+  border-radius: 4px;
+  position: relative;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  margin-top: 10px;
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  width: 80%;
+  max-width: 500px;
+  z-index: 1000;
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.img-product {
+  width: 10%;
+  height: 10%;
+  border-radius: 4px;
+  margin-right: 10px;
+  margin-left: 45%;
+}
+
+.search-section {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
